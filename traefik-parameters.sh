233391f30bc8bf910e2c93113cdfbb71bf21674b
Jose Riguera <jriguera@gmail.com>
2020-03-25 10:53:41 +0100

    fix default version

--- traefik-parameters.sh
+++ traefik-parameters.sh
@@ -1,5 +1,5 @@
 # Variables
 
 # default versions
-VERSION_TRAEFIK="${VERSION_TRAEFIK:-1.50.0}"
+VERSION_TRAEFIK="${VERSION_TRAEFIK:-2.1.9}"
 DOWNLOAD_URL_TRAEFIK="https://github.com/containous/traefik/releases/download/v${VERSION_TRAEFIK}/traefik_v${VERSION_TRAEFIK}_linux_amd64.tar.gz"
